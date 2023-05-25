f5d54c26b08aca3b3c6b2ce0992d11f24be6e8f4
aleymarr <aleyba2002@gmail.com>
2023-05-25 19:38:25 +0100

    Feat(Personaje):Creando los getters y setters para esta clase

--- src/com/salesianos/Personaje/Personaje.java
+++ src/com/salesianos/Personaje/Personaje.java
@@ -12,5 +12,27 @@ public class Personaje {
         this.fuerza = fuerza;
     }
 
+    public int getSalud() {
+        return salud;
+    }
 
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
 }
