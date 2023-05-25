4aafc76bd32584813cba970913ac088a3cc2e5b6
aleymarr <aleyba2002@gmail.com>
2023-05-25 20:33:34 +0100

    Fix(Personaje):Añadiendo el metodo moverse que estaba vacio

--- src/com/salesianos/Zombie/Zombie.java
+++ src/com/salesianos/Zombie/Zombie.java
@@ -4,13 +4,12 @@ import com.salesianos.MobHostil.MobHostil;
 
 public class Zombie extends MobHostil {
 
-
     public Zombie() {
         super(10, 4);
     }
 
     @Override
     public void moverse() {
-
+        System.out.println("El Zombie se mueve hacia el personaje.");
     }
 }
