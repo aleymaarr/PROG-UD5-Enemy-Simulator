dbcc1c8c76165a7eaafa9a1576f9cca15d363555
aleymarr <aleyba2002@gmail.com>
2023-05-25 20:45:23 +0100

    Feat(Personaje):Creando la clase Oveja con sus atributos y el metodo moverse

---
+++ src/com/salesianos/Oveja/Oveja.java
@@ -0,0 +1,16 @@
+package com.salesianos.Oveja;
+
+import com.salesianos.MobPacifico.MobPacifico;
+
+public class Oveja extends MobPacifico {
+    public Oveja() {
+        super(5);
+    }
+
+    @Override
+    public void moverse() {
+
+    }
+
+
+}
