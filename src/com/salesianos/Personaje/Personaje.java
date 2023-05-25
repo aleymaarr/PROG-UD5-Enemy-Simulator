b732cb9d66d8b2c93daa6ccfea064e8b928c3cb9
aleymarr <aleyba2002@gmail.com>
2023-05-25 19:36:01 +0100

    Feat(Personaje):Creando los atributos y contructores para esta clase

---
+++ src/com/salesianos/Personaje/Personaje.java
@@ -0,0 +1,16 @@
+package com.salesianos.Personaje;
+
+public class Personaje {
+
+    private int salud;
+    private int defensa;
+    private int fuerza;
+
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12;
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+    }
+
+
+}
