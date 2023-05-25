69dcba250d8dd2b7cc4d02cdd8c8680565aa0a74
aleymarr <aleyba2002@gmail.com>
2023-05-25 20:50:52 +0100

    Feat(Personaje):Creando la instancia de los objectos que vamos a utlizar y hacediendo a ellas con sus metodos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import com.salesianos.Enderman.Enderman;
+import com.salesianos.Oveja.Oveja;
+import com.salesianos.Zombie.Zombie;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Zombie zombie = new Zombie();
+        zombie.moverse();
+
+        Enderman enderman = new Enderman();
+        enderman.moverse();
+
+        Oveja oveja = new Oveja();
+        oveja.moverse();
+
 
     }
 }
