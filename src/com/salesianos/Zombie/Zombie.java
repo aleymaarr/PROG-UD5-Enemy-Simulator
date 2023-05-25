7c272717df1ca8f38d249feaaf4015e55656eecb
aleymarr <aleyba2002@gmail.com>
2023-05-25 20:32:23 +0100

    Feat(Personaje):Creando la clase Zombie que va extender de la clase MobHostilt

---
+++ src/com/salesianos/Zombie/Zombie.java
@@ -0,0 +1,16 @@
+package com.salesianos.Zombie;
+
+import com.salesianos.MobHostil.MobHostil;
+
+public class Zombie extends MobHostil {
+
+
+    public Zombie() {
+        super(10, 4);
+    }
+
+    @Override
+    public void moverse() {
+
+    }
+}
