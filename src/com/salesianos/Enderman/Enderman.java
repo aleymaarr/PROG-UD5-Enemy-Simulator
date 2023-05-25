00f459674cb0b58e9b1167ef3cd1c075d96d6a90
aleymarr <aleyba2002@gmail.com>
2023-05-25 20:38:50 +0100

    Feat(Personaje):Creando la clase Enderman y pasandole su atributos y el metodo moverse

---
+++ src/com/salesianos/Enderman/Enderman.java
@@ -0,0 +1,15 @@
+package com.salesianos.Enderman;
+
+import com.salesianos.MobHostil.MobHostil;
+
+public class Enderman extends MobHostil {
+
+    public Enderman() {
+        super(14, 6);
+    }
+
+    @Override
+    public void moverse() {
+
+    }
+}
