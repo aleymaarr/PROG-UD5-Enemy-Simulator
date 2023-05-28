a3098c617293f93a755d7baa3b3ee0ccfbd00b83
aleymarr <aleyba2002@gmail.com>
2023-05-28 18:32:45 +0100

    Feat(menu):Creando un bucle while con las condiciones en la que nos pide y al finalizaz mostrar un mensaje de victoria o ganador

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,31 @@ public class Main {
         int turnos = 0;
         Random random = new Random();
 
+        while(personaje.getSalud() > 0 && enemigo.getSalud() > 0){
 
+            int danoPersonaje = personaje.atacar();
+            enemigo.recibirAtaque(danoPersonaje);
+
+            if(enemigo.getSalud() <= 0){
+                break;
+            }
+
+            int accionEnemigo = random.nextInt(2);
+
+            if (accionEnemigo == 0){
+                int danoEnemigo = enemigo.atacar();
+                personaje.recibirAtaque(danoEnemigo);
+            }else{
+                enemigo.moverse();
+            }
+
+            turnos++;
+        }
+
+        if (personaje.getSalud() <= 0){
+            System.out.println("¡Has sido derrotado! El enemigo te han vencido en" + turnos + "turnos");
+        }else{
+            System.out.println("!Has ganado¡ Has derrotado al enemigo en" + turnos + "turnos");
+        }
     }
 }
