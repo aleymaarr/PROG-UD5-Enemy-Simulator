904af875ea10ddc537dddf111f10f3c68ccfa70c
aleymarr <aleyba2002@gmail.com>
2023-05-28 18:28:29 +0100

    Feat(menu):Instanciado el mobhostil para asignarle un metodo y creando un atributo entero y el metodo random

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import com.salesianos.MobHostil.MobHostil;
 import com.salesianos.Personaje.Personaje;
+
+import java.util.Random;
 
 public class Main {
 
@@ -8,7 +11,12 @@ public class Main {
 
         int fuerza = obtenerFuerzaArma();
         int defensa = obtenerDefensaArmadura();
+
         Personaje personaje = new Personaje(fuerza,defensa);
+        MobHostil enemigo = generarEnemigoHostil();
+
+        int turnos = 0;
+        Random random = new Random();
 
 
     }
