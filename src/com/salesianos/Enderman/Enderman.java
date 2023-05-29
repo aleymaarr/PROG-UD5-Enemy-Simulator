987da4221b06c339217e2f8e80dc66294f687113
aleymarr <aleyba2002@gmail.com>
2023-05-29 18:26:19 +0100

    Fix(Personaje):Añadiendo cosas al la clase MobHostil

--- src/com/salesianos/Enderman/Enderman.java
+++ src/com/salesianos/Enderman/Enderman.java
@@ -12,4 +12,8 @@ public class Enderman extends MobHostil {
         System.out.println("El Enderman se teletransporta detrás.");
     }
 
+    @Override
+    public String getNombre() {
+        return "Enderman";
+    }
 }
